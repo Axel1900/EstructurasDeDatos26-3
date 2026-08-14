02e7320b142cda3663d7ef5462bc6421c9c1915e
Axel1900 <axel.gomez@saei.mx>
2026-08-14 13:50:30 -0600

    Se termino la linked list

--- EstructurasDeDatos26-3/LinkedList.cpp
+++ EstructurasDeDatos26-3/LinkedList.cpp
@@ -1,1 +1,70 @@
 #include "LinkedList.h"
+#include <iostream>
+
+LinkedList::LinkedList()
+{
+	_first = nullptr;
+	_size = 0;
+}
+
+void LinkedList::Add(int dato)
+{
+	Node* nuevo = new Node();
+	nuevo->data = dato;
+	nuevo->next = nullptr;
+
+	if (_first == nullptr)
+	{
+		_first = nuevo;
+	}
+	else
+	{
+		Node* actual = _first;
+		while (actual->next != nullptr)
+		{
+			actual = actual->next;
+		}
+		actual->next = nuevo;
+	}
+	_size++;
+}
+
+int LinkedList::GetAt(int index)
+{
+	if (index < 0 || index >= _size)
+	{
+		return -1;
+	}
+
+	Node* actual = _first;
+	for (int i = 0; i < index; i++)
+	{
+		actual = actual->next;
+	}
+	return actual->data;
+}
+
+int LinkedList::GetSize()
+{
+	return _size;
+}
+
+void LinkedList::Print()
+{
+	Node* actual = _first;
+	while (actual != nullptr)
+	{
+		actual = actual->next;
+	}
+}
+
+LinkedList::~LinkedList()
+{
+	Node* actual = _first;
+	while (actual != nullptr)
+	{
+		Node* siguiente = actual->next;
+		delete actual;
+		actual = siguiente;
+	}
+}
--- EstructurasDeDatos26-3/LinkedList.h
+++ EstructurasDeDatos26-3/LinkedList.h
@@ -2,16 +2,22 @@
 class LinkedList
 {
 private:
+	struct Node
+	{
+		int data;
+		Node* next;
+	};
+
+	Node* _first;
 	int _size;
 
 public:
 	LinkedList();
-	void Add(int dato);
-	int GetAt(int index);
+	void Add(int p_dato);
+	int GetAt(int p_index);
 	int GetSize();
 	void Print();
 	void InsertionSort();
 	void SelectionSort();
 	~LinkedList();
 };
-
